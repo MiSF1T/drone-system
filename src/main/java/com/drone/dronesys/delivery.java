859928f41c190d1f49b375bff63a8d635952af6d
Raunak <raunakbagh@gmail.com>
2022-10-18 13:49:19 +0200

    Initial commit for drone system

---
+++ src/main/java/com/drone/dronesys/delivery.java
@@ -0,0 +1,2 @@
+package com.drone.dronesys;public class delivery {
+}
---
+++ src/test/java/com/drone/dronesys/DroneSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drone.dronesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
